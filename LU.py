d83e25ca661fdbe3f98a616116647d0b01823268
dankiy <iorsadg@gmail.com>
2019-04-16 18:56:14 +0300

    added generate, det and solve

--- LU.py
+++ LU.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math
 
 class LU:
 
-    def __init__(self, matrix, b):
+    def __init__(self, C, b):
         """
         self.L = np.eye(len(matrix)).map(
             lambda i, j: [matrix[i, j] / matrix[i, j - 1] for j in range(1, len(matrix) - i)] for i in
@@ -10,16 +11,26 @@ class LU:
         self.b = np.array(matrix[:, 0].map(lambda i: i * b[i]))
         self.U = [[matrix[i, j](1 - self.L[i, j]) for i in range(0, len(matrix))] for j in range(0, len(matrix))]
         """
-        n = len(matrix)
-        self.C = matrix
+        n = len(C)
+        P = np.eye(n)
         for i in range(n):
-            self.C, b = swap(self.C, b)
-        if self.C[0][0] != 0:
-            for j in range(i+1, n):
-                self.C[j][i] /= self.C[i][i]
-                for k in range(i+1, n):
-                    self.C[j][k] -= self.C[j][i] * self.C[i][k]
-        self.C -= np.eye(len(matrix))
+            pivotValue = 0
+            pivot = -1
+            for row in range(i, n):
+                if (math.fabs(C[row][i]) > pivotValue):
+                    pivotValue = math.fabs(C[row][i])
+                    pivot = row
+            if pivotValue != 0:
+                C[[i, pivot]] = C[[pivot, i]]
+                P[[i, pivot]] = P[[pivot, i]]
+                b[i], b[pivot] = b[pivot], b[i]
+                for j in range(i+1, n):
+                    C[j][i] /= C[i][i]
+                    for k in range(i+1, n):
+                        C[j][k] -= C[j][i] * C[i][k]
+
+        self.C = C
+        self.P = P
         self.b = b
 
     def det(self):
@@ -31,19 +42,27 @@ class LU:
         solution = -self.b
         for i in range(2, len(solution)+1):
             for j in range(0, len(solution)):
-                solution[len(solution)-i] -= solution[j]*np.tril(self.C)[j][i-2]
+                solution[len(solution)-i] -= solution[j]*(np.tril(self.C,-1)+np.eye(len(self.C)))[j][i-2]
         return solution
 
     def show(self):
-        print(np.tril(self.C))
+        print(np.tril(self.C,-1)+np.eye(len(self.C)))
         print()
         print(np.triu(self.C))
+        print()
+        print(self.P)
         print()
         print(self.b)
         print()
 
-def swap(matrix, b):
-    x = np.argmax(matrix[0])
+    def cond(self):
+        x = self.solve()
+
+
+def swap(matrix, P, b, i):
+    n = len(matrix)
+    x = np.argmax([math.fabs(matrix[row][i]) for row in range(i, n)])
     matrix[[0, x]] = matrix[[x, 0]]
+    P[[0, x]] = P[[x, 0]]
     b[0], b[x] = b[x], b[0]
-    return matrix, b
+    return matrix, P, b
--- calc.py
+++ calc.py
@@ -1,7 +1,14 @@
 import LU
 import Generate
+import numpy as np
 
-matrix, b = Generate.Generate(5)
+#matrix, b = Generate.Generate(5)
+matrix = np.ones(5,5)
+b = np.array([1,2,3,4,5], dtype=float)
+print("matrix:")
+print(matrix)
+print("b:")
+print(b)
 luMatrix = LU.LU(matrix, b)
 print("show: ")
 luMatrix.show()
@@ -9,4 +16,8 @@ print("solve: ")
 print(luMatrix.solve())
 print("det: ")
 print(luMatrix.det())
+print("L*U:")
+print(np.multiply(np.tril(luMatrix.C,-1)+np.eye(len(luMatrix.C)), np.triu(luMatrix.C)))
+print("P*A:")
+print(np.multiply(luMatrix.P, matrix))
 
