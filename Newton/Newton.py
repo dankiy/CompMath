3e4164d457e3740367ef843ddbb07ddd0144eed9
dankiy <iorsadg@gmail.com>
2019-05-11 00:29:48 +0300

    added modified and hybrid method

--- Newton/Newton.py
+++ Newton/Newton.py
@@ -1,20 +1,18 @@
 import numpy as np
 
-def Newton(funcSystem, x0, k: int = 0, eps: float = 1e-9, lim: int = 1e3):
+def Newton(funcSystem, x0, k: int = 1, eps: float = 1e-9, lim: int = 1e3):
     x = x0
     x_prev = x0 + [2 * eps for i in range(len(x0))]
     i = 0
-    if k == 0:
-        while max([abs(x[i] - x_prev[i]) for i in range(len(x))]) >= eps and i < lim:
+    if k == -1:
+        W = Jacobi(funcSystem, x)
+    while max([abs(x[i] - x_prev[i]) for i in range(len(x))]) >= eps and i < lim:
+        if k != -1 and i % k == 0:
             W = Jacobi(funcSystem, x)
-            x_prev = x
-            f = np.array([funcSystem[i](x) for i in range(len(x))])
-            x = x - np.dot(np.linalg.inv(W),f)
-            i += 1
-
-    else:
-        pass
-        ##TODO
+        x_prev = x
+        f = np.array([funcSystem[i](x) for i in range(len(x))])
+        x = x - np.dot(np.linalg.inv(W),f)
+        i += 1
     return x, i
 
 def Jacobi(funcSystem, x0):
